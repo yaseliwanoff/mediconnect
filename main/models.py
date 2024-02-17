49889f3f435a8f410cd7d9190c0a7d22cdaff3f6
miro <144848251+yaseliwanoff@users.noreply.github.com>
2024-02-17 18:56:44 +0300

    еще один тестовый пуш

--- main/models.py
+++ main/models.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 
-# Для тестового пуша, удалить
+
 class SpecializationCategory(models.Model):
     specialization_title = models.CharField(verbose_name='Specialization title')
 
