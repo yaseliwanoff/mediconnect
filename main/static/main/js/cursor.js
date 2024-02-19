05a69da3ee368d6eed7e48ce8ebd4781bd9b0e78
miro <144848251+yaseliwanoff@users.noreply.github.com>
2024-02-19 22:55:28 +0300

    reload js file

--- main/static/main/js/cursor.js
+++ main/static/main/js/cursor.js
@@ -1,22 +1,23 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const cursor = document.createElement('div');
-    cursor.classList.add('custom-cursor');
-    document.body.appendChild(cursor);
+  const cursor = document.createElement('div');
+  cursor.classList.add('custom-cursor');
+  document.body.appendChild(cursor);
 
-    function moveCursor(x, y) {
-      cursor.style.left = x + 'px';
-      cursor.style.top = y + 'px';
-    }
+  function moveCursor(x, y) {
+    cursor.style.left = x + 'px';
+    cursor.style.top = y + 'px';
+  }
 
-    document.addEventListener('mousemove', (e) => {
-      moveCursor(e.clientX, e.clientY);
-    });
-
-    document.addEventListener('mousedown', () => {
-      cursor.classList.add('pressed');
-    });
-
-    document.addEventListener('mouseup', () => {
-      cursor.classList.remove('pressed');
-    });
+  // Добавляем обработчик движения мыши
+  document.addEventListener('mousemove', (e) => {
+    moveCursor(e.clientX, e.clientY);
   });
+
+  document.addEventListener('mousedown', () => {
+    cursor.classList.add('pressed');
+  });
+
+  document.addEventListener('mouseup', () => {
+    cursor.classList.remove('pressed');
+  });
+})
