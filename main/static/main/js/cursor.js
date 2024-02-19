1646ffcb81aeefc17ffb1ebb761ba7504559150d
miro <144848251+yaseliwanoff@users.noreply.github.com>
2024-02-19 18:52:51 +0300

    creating js file for cursor in the website

---
+++ main/static/main/js/cursor.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cursor = document.createElement('div');
+    cursor.classList.add('custom-cursor');
+    document.body.appendChild(cursor);
+
+    function moveCursor(x, y) {
+      cursor.style.left = x + 'px';
+      cursor.style.top = y + 'px';
+    }
+
+    document.addEventListener('mousemove', (e) => {
+      moveCursor(e.clientX, e.clientY);
+    });
+
+    document.addEventListener('mousedown', () => {
+      cursor.classList.add('pressed');
+    });
+
+    document.addEventListener('mouseup', () => {
+      cursor.classList.remove('pressed');
+    });
+  });
