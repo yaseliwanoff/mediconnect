29429aec6a51c4dad0f859fdf5c6bad75a0d8927
giorgi <giorgi.oniani.04@gmail.com>
2024-02-21 12:58:49 +0400

    Fixed bug in user appointments filter form, renamed page from My appointments to My page

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from .views import DoctorInfo, DoctorListView, RegisterFormView, LoginUserView, my_logout_view,\
-    UserAppointmentsListView, Main, ContactView
+    UserPageView, Main, ContactView
 
 
 urlpatterns = [
@@ -10,7 +10,7 @@ urlpatterns = [
     path('register/', RegisterFormView.as_view(), name='Register'),
     path('login/', LoginUserView.as_view(), name='Login'),
     path('logout/', my_logout_view, name='Logout'),
-    path('my-appointments/', UserAppointmentsListView.as_view(), name='UserAppointments'),
+    path('my-page/', UserPageView.as_view(), name='UserPage'),
     path('doctor-list/', DoctorListView.as_view(), name='DoctorList'),
     path('contacts/', views.ContactView, name='Contact')
 ]
